7dc0a9c7e9390ca408bf485553299e97d3c3767f
chs0218 <39898936+chs0218@users.noreply.github.com>
2024-09-06 21:30:05 +0900

    Build.cs 파일 업데이트
    EnhancedInput 모듈 사용을 위해 의존성 추가, ByTheSea를 PublicIncludPaths에 추가

--- ByTheSea/Source/ByTheSea/ByTheSea.Build.cs
+++ ByTheSea/Source/ByTheSea/ByTheSea.Build.cs
@@ -7,10 +7,17 @@ public class ByTheSea : ModuleRules
 	public ByTheSea(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+        PublicIncludePaths.AddRange(new string[] { "ByTheSea" });
+
+        PublicDependencyModuleNames.AddRange(new string[] {
+            "Core",
+            "CoreUObject",
+            "Engine",
+            "InputCore",
+            "EnhancedInput"});
+
+        PrivateDependencyModuleNames.AddRange(new string[] {  });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
