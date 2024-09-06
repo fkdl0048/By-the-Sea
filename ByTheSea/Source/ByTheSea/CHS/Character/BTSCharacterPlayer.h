c613f18c884ab2024cd5f829c4de463872f09656
chs0218 <39898936+chs0218@users.noreply.github.com>
2024-09-07 05:21:26 +0900

    물고기 튀어오르기 개발

--- ByTheSea/Source/ByTheSea/CHS/Character/BTSCharacterPlayer.h
+++ ByTheSea/Source/ByTheSea/CHS/Character/BTSCharacterPlayer.h
@@ -6,6 +6,14 @@
 #include "CHS/Character/BTSCharacterBase.h"
 #include "BTSCharacterPlayer.generated.h"
 
+UENUM()
+enum class ECharacterPlayerState : uint8
+{
+	IDLE,
+	FLUTTER,
+	WAITFORIDLE
+};
+
 /**
  * 
  */
@@ -17,7 +25,15 @@ public:
 	ABTSCharacterPlayer();
 
 	virtual void BeginPlay() override;
+	virtual void Landed(const FHitResult& Hit) override;
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	
+public:
+	FORCEINLINE void SetState(ECharacterPlayerState NewState) { CurState = NewState; }
+	FORCEINLINE ECharacterPlayerState GetCurState() { return CurState; }
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = State)
+	ECharacterPlayerState CurState = ECharacterPlayerState::FLUTTER;
 
 // 카메라 부분
 protected:
